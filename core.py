a7a9224f79bd91dbbc88101c4193275ad34d4b0a
Roman.Tsukanov <Roman.Tsukanov@x5.ru>
2021-11-15 01:18:34 +0300

    several changes

--- core.py
+++ core.py
@@ -2,6 +2,7 @@ import datetime
 import logging
 import yaml
 import os
+from collections import defaultdict
 
 NUM_PLAYERS = yaml.safe_load(open('./config.yaml', 'r'))['num_players']
 NUM_EXTRA_PLAYERS = yaml.safe_load(open('./config.yaml', 'r'))['num_extra_players']
@@ -9,8 +10,10 @@ NUM_EXTRA_PLAYERS = yaml.safe_load(open('./config.yaml', 'r'))['num_extra_player
 def read_parameter(param_name):
     if os.getenv(param_name.upper()):
         return os.getenv(param_name.upper())
-    else:
+    elif yaml.safe_load(open('./config.yaml', 'r'))[param_name]:
         return yaml.safe_load(open('./config.yaml', 'r'))[param_name]
+    else:
+        return ''
 
 
 class PollSession:
@@ -28,18 +31,32 @@ class PollSession:
 
         self.game_date = game_date # mb make a propery?
 
-        self.player_set = {}
-        self.extra_player_set = {}
+        self.player_set = defaultdict(int)
+        self.extra_player_set = defaultdict(int)
 
     def __repr__(self):
         return f'{self.session_start_time}__{self.session_end_time} with {list(self.player_set)} players'
 
+
+    @property
+    def is_full(self):
+        print(sum(self.player_set.values()))
+        print(NUM_PLAYERS)
+
+
+        return sum(self.player_set.values()) == NUM_PLAYERS
+
     def remove_player_from_session(self, player):
         if player in self.player_set:
             logging.info(f'Removing {player} from player-set')
-            self.player_set.pop(player)
-            if len(self.extra_player_set) > 0:
-                self.player_set[list(self.extra_player_set)[0]] = None
+            if self.player_set[player] > 1:
+                self.player_set[player] -= 1
+
+            else:
+                self.player_set.pop(player)
+
+            # if len(self.extra_player_set) > 0:
+            #     self.player_set[] = None
 
         elif player in self.extra_player_set:
             logging.info(f'Removing {player} from extra player set')
@@ -54,24 +71,41 @@ class PollSession:
 
 
     def add_player_to_session(self, player):
-        if len(self.player_set) < NUM_PLAYERS:
+        if sum(self.player_set.values()) < NUM_PLAYERS:
             logging.info(f'Adding {player} to main player set')
-            self.player_set[player] = None
+            self.player_set[player] += 1
+
 
         elif (len(self.player_set) >= NUM_PLAYERS) and (len(self.player_set) <= NUM_EXTRA_PLAYERS):
             logging.info(f'Adding {player} to extra set')
-            self.extra_player_set[player] = None
+            self.extra_player_set[player] += 1
 
         else:
             logging.info(f'Queue is full.')
 
 
     def render_status(self):
-        return f''' 
-        *Session starts at: {self.session_start_time.date()} for game: {self.game_date}*
-        ''' + ' - '.join(['@' + player for player in list(self.player_set)]) + '\n\n' + ' * '.join(['@' + player for
-                                                                                                    player in list(
-                self.extra_player_set)])
+
+
+        emoji_status = u'\U000027a1' if not self.is_full else u'\U0000274c'
+        print('FULL?: ', self.is_full)
+
+        header = emoji_status + f'* Session starts at: {self.session_start_time.date()} for game: {self.game_date}* \n\n'
+
+        rows = [header]
+
+        for n, user_id in enumerate(self.player_set):
+            print(f'USER ID : {user_id}')
+            # user_id = user_id.replace("_", "\_")
+            row = f'{n + 1}. @{user_id}' if self.player_set[user_id] == 1 else f'{n + 1}. @{user_id}' + f' (+{self.player_set[user_id] - 1})'
+            row = row.replace("_", "\_") # in order to proceed with markup
+            rows.append(row)
+
+        print(self.player_set)
+        print('SOME USEFUL: ' + '\n'.join(rows))
+
+        return '\n'.join(rows)
+
 
 
 
--- server_v2.py
+++ server_v2.py
@@ -1,21 +1,23 @@
 import telebot
-import yaml
 import logging
 import sys
-import json
 from core import PollSession, read_parameter
+import argparse
 import datetime
 import psycopg2
 from flask import Flask, request
 import re
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--debug')
+
 TOKEN = read_parameter('token')
 SESSION_ADMINS = read_parameter('admins')
 GROUP_ID = read_parameter('group_chat_id')
-
 PG_HOST = read_parameter('DATABASE_URL')
+print(PG_HOST)
 
-print('HOST IS: ', PG_HOST)
+# print('HOST IS: ', PG_HOST)
 PG_PASSWORD = read_parameter('pg_password')
 PG_DB = read_parameter('pg_db')
 PG_USER = read_parameter('pg_user')
@@ -24,20 +26,58 @@ MATCHTIME = datetime.time(int(read_parameter('matchtime')), 0, 0, 0)
 
 server = Flask(__name__)
 
-_, user, db_token, host, port, db = re.findall(r'(\w+)://(\w+):(\w+)@(.+):(\d+)\/(\w+)', PG_HOST)[0]
+def plus_minus_markup():
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(telebot.types.InlineKeyboardButton("+", callback_data="+"),
+               telebot.types.InlineKeyboardButton("-", callback_data="-"))
+    return markup
 
-import os
+if PG_HOST:
+    _, user, db_token, host, port, db = re.findall(r'(\w+)://(\w+):(\w+)@(.+):(\d+)\/(\w+)', PG_HOST)[0]
+else:
+    user = PG_USER
+    db_token = PG_PASSWORD
+    db = PG_DB
+    host = 'localhost'
 
 log = logging.getLogger('server')
 log.setLevel(logging.INFO)
 
+def group(function):
+    def wrapper():
+        func = function(lambda m: m.chat.type == 'group')
+        return func
+    return wrapper
+
+SESSION_ONLY = lambda m: m.from_user.username in SESSION_ADMINS
+GROUP_ONLY = lambda m: m.chat.type == 'group'
+ALWAYS_TRUE = lambda m: True
+
+
 class Run:
-    def __init__(self):
+    def __init__(self, debug=False):
+
+        self.debug = debug
         logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
         self.log = logging.getLogger('run_instance')
-        self.log.setLevel(logging.INFO)
+        self.log.setLevel(logging.INFO if not debug else logging.DEBUG)
         self.current_poll_session = None
+        self.current_poll_session_message = None
+
         self.bot = telebot.TeleBot(TOKEN, parse_mode='MARKDOWN')
+        self.bot.remove_webhook()
+
+        self.callback_handlers = {self.proceed_group_pluses_from_callback: {'func': ALWAYS_TRUE}}
+
+        self.message_handlers = {self.start_new_poll: {'func': SESSION_ONLY,
+                               'commands': ['start_new_poll']},
+         self.check_current_poll: {'commands': ['check_current_poll'],
+                                   'func': SESSION_ONLY},
+         self.end_current_poll: {'commands': ['end_current_poll'],
+                                 'func': SESSION_ONLY},
+         self.proceed_group_pluses: {'func': GROUP_ONLY}
+         }
 
         self.conn = psycopg2.connect(
             host=host,
@@ -47,79 +87,118 @@ class Run:
 
         self.cursor = self.conn.cursor()
 
-        @self.bot.message_handler(commands=['start_new_poll'], func=lambda m: m.from_user.username in SESSION_ADMINS)
-        def start_new_poll(message):
-            now = datetime.datetime.now()
-            self.log.info(message)
-            if self.current_poll_session is None or self.current_poll_session.session_end_time <= now:
-                # TODO: try to fetch latest session
+        # self.check_current_status()
+        self.register_message_handlers(self.message_handlers)
+        self.register_callback_handlers(self.callback_handlers)
 
-                matchday = self._find_closest_game_date(now)
-                matchtime = datetime.datetime.combine(matchday, MATCHTIME)
-
-                self.current_poll_session = PollSession(session_start_time=now,
-                                                     session_end_time=matchtime,
-                                                     game_date=matchday,
-                                                     is_closed=False)
-
-                self.log.info(f'Creating new PollSession. \n Will active from '
-                              f'{self.current_poll_session.session_start_time} to'
-                              f' {self.current_poll_session.session_end_time}')
-
-                self.bot.send_message(chat_id=GROUP_ID, text=str(self.current_poll_session))
+    def register_message_handlers(self, handlers):
+        for handler, kwargs in handlers.items():
+            self.bot.add_message_handler(self.bot._build_handler_dict(handler, **kwargs))
 
 
-        @self.bot.message_handler(commands=['check_current_poll'], func=lambda m: m.from_user.username in
-                                                                                  SESSION_ADMINS)
-        def check_current_poll(message):
+    def register_callback_handlers(self, handlers):
+        for handler, kwargs in handlers.items():
+            self.bot.add_callback_query_handler(self.bot._build_handler_dict(handler, **kwargs))
+
+
+    def start_new_poll(self, message):
+        now = datetime.datetime.now()
+        self.log.info(message)
+        if self.current_poll_session is None or self.current_poll_session.session_end_time <= now:
+            # TODO: try to fetch latest session
+
+            matchday = self._find_closest_game_date(now)
+            matchtime = datetime.datetime.combine(matchday, MATCHTIME)
+
+            self.current_poll_session = PollSession(session_start_time=now,
+                                                    session_end_time=matchtime,
+                                                    game_date=matchday,
+                                                    is_closed=False)
+
+            self.log.info(f'Creating new PollSession. \n Will active from '
+                          f'{self.current_poll_session.session_start_time} to'
+                          f' {self.current_poll_session.session_end_time}')
+
+            msg = self.bot.send_message(chat_id=GROUP_ID, text=self.current_poll_session.render_status(),
+                                  reply_markup=plus_minus_markup())
+
+            self.log.debug(f'Saving current message id: {msg.message_id}')
+            self.current_message_id = msg.message_id
+
+            self.bot.pin_chat_message(chat_id=GROUP_ID, message_id=self.current_message_id)
+
+
+
+    def check_current_poll(self, message):
+        log.info(self.current_poll_session)
+        self.bot.send_message(chat_id=GROUP_ID,
+                              text=self.current_poll_session.render_status())
+
+
+    def end_current_poll(self, message):
+        if self.current_poll_session:
             log.info(self.current_poll_session)
-            self.bot.send_message(chat_id=GROUP_ID,
-                                  text=self.current_poll_session.render_status())
+            self.bot.send_message(chat_id=GROUP_ID, text=str(self.current_poll_session) + "is closed.")
 
 
-        @self.bot.message_handler(commands=['end_current_poll'], func=lambda m: m.from_user.username in
-                                                                                  SESSION_ADMINS)
-        def end_current_poll(message):
-            if self.current_poll_session:
-                log.info(self.current_poll_session)
-                self.bot.send_message(chat_id=GROUP_ID, text=str(self.current_poll_session) + "is closed.")
+    def proceed_group_pluses_from_callback(self, query):
+
+        now = datetime.datetime.now()
+
+        if self.current_poll_session and query.data and query.data == '+':
+
+            username = query.from_user.username
+
+            self.current_poll_session.add_player_to_session(username)
+            self._add_player_to_db_session(self.current_poll_session, username, out=False, now=now)
+            self.bot.edit_message_text(text=self.current_poll_session.render_status(),
+                                       chat_id=GROUP_ID,
+                                       message_id=self.current_message_id,
+                                       reply_markup=plus_minus_markup())
+
+        if self.current_poll_session and query.data and query.data == '-':
+
+            username = query.from_user.username
+
+            self.current_poll_session.remove_player_from_session(username)
+
+            self._add_player_to_db_session(self.current_poll_session, username, out=True, now=now)
+            self.bot.edit_message_text(text=self.current_poll_session.render_status(),
+                                       chat_id=GROUP_ID,
+                                       message_id=self.current_message_id,
+                                       reply_markup=plus_minus_markup())
 
 
+    def proceed_group_pluses(self, message):
+        if self.current_poll_session and message.text and message.text == '+':
 
-        @self.bot.message_handler(func=lambda m: m.chat.type == 'group')
-        def proceed_group_pluses(message):
-            if self.current_poll_session and message.text and message.text == '+':
+            now = datetime.datetime.now()
+            username = message.from_user.username
 
-                now = datetime.datetime.now()
-                username = message.from_user.username
+            self.current_poll_session.add_player_to_session(username)
+            self._add_player_to_db_session(self.current_poll_session, username, out=False, now=now)
 
-                self.current_poll_session.add_player_to_session(username)
-                print(self.current_poll_session.player_set)
-                # if not username in self.current_poll_session.player_set:
-                #     print(1111)
-                self._add_player_to_db_session(self.current_poll_session, username, out=False, now=now)
+        if self.current_poll_session and message.text and message.text == '-':
+            now = datetime.datetime.now()
+            username = message.from_user.username
 
-            if self.current_poll_session and message.text and message.text == '-':
-                now = datetime.datetime.now()
-                username = message.from_user.username
-
-                self.current_poll_session.remove_player_from_session(username)
-                if username in self.current_poll_session.player_set:
-                    self._add_player_to_db_session(self.current_poll_session, username, out=True, now=now)
-
-        @server.route('/' + TOKEN, methods=['POST'])
-        def getMessage():
-            json_string = request.get_data().decode('utf-8')
-            update = telebot.types.Update.de_json(json_string)
-            self.bot.process_new_updates([update])
-            return "!", 200
-
-        @server.route("/")
-        def webhook():
-            print('Im in webhook')
-            self.bot.remove_webhook()
-            self.bot.set_webhook(url='https://doweplayfootball.herokuapp.com/' + TOKEN)
-            return "!", 200
+            self.current_poll_session.remove_player_from_session(username)
+            if username in self.current_poll_session.player_set:
+                self._add_player_to_db_session(self.current_poll_session, username, out=True, now=now)
+        #
+        # @server.route('/' + TOKEN, methods=['POST'])
+        # def getMessage():
+        #     json_string = request.get_data().decode('utf-8')
+        #     update = telebot.types.Update.de_json(json_string)
+        #     self.bot.process_new_updates([update])
+        #     return "!", 200
+        #
+        # @server.route("/")
+        # def webhook():
+        #     print('Im in webhook')
+        #     self.bot.remove_webhook()
+        #     self.bot.set_webhook(url='https://doweplayfootball.herokuapp.com/' + TOKEN)
+        #     return "!", 200
 
 
     def _find_closest_game_date(self, time, matchday=5):
@@ -148,9 +227,13 @@ class Run:
 
 
 if __name__ == "__main__":
-    try:
-        r = Run()
-        server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)), debug=False)
+    # try:
+    args = parser.parse_args()
+    # r = Run(debug=args.debug)
+    r = Run(debug=True)
+    r.bot.remove_webhook()
+    r.bot.infinity_polling()
+    # server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)), debug=False)
 
-    except Exception as e:
-        print(e)
+    # except Exception as e:
+    #     print(e)
