b62ce20ad1870c36d58eec71158f0545d05ef19a
rvtsukanov <Roman.Tsukanov@x5.ru>
2024-04-21 17:27:48 +0300

    add access and secret keys to terraform init in cicd

--- terraform/bot.tf
+++ terraform/bot.tf
@@ -14,8 +14,8 @@ terraform {
 
     skip_region_validation      = true
     skip_credentials_validation = true
-#    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
-#    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
 
   }
   required_version = ">= 0.13"
