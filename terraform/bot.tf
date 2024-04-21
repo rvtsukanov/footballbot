8aa8a588f8ec9c323009f0c9aed7e7391fc4dcf0
rvtsukanov <Roman.Tsukanov@x5.ru>
2024-04-21 17:24:43 +0300

    add access and secret keys to terraform init in cicd

--- terraform/bot.tf
+++ terraform/bot.tf
@@ -4,6 +4,18 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
+  backend "s3" {
+    endpoint = "https://storage.yandexcloud.net"
+    bucket = "dwpf-storage-terraform"
+    region = "ru-central1"
+    key    = "states/state.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+#    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+#    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+  }
   required_version = ">= 0.13"
 }
 
